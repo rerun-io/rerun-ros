f0e63416e10b21648b41c2c6835bc25739e5e600
Esteve Fernandez <esteve@apache.org>
2024-08-12 16:01:57 +0200

    Fix dependencies
    Signed-off-by: Esteve Fernandez <esteve@apache.org>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-//! Example of a Rust binary.
+use std::env;
 
-fn main() {
-    println!("Hello, PROJ_NAME!");
+use anyhow::{Error, Result};
+
+fn main() -> Result<(), Error> {
+    let context = rclrs::Context::new(env::args())?;
+
+    let node = rclrs::create_node(&context, "minimal_subscriber")?;
+
+    rclrs::spin(node).map_err(|err| err.into())
 }
