995e8947799cfead79293eada2df48670075fa50
Esteve Fernandez <esteve@apache.org>
2024-08-16 18:04:39 +0200

    Use local fork for bridge until the generic subscription API is ready
    Signed-off-by: Esteve Fernandez <esteve@apache.org>

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,14 @@ fn main() -> Result<(), Error> {
 
     let node = rclrs::create_node(&context, "minimal_subscriber")?;
 
+    let generic_subscription = node.create_generic_subscription(
+        "topic",
+        "std_msgs/msg/String");
+        rclrs::QOS_PROFILE_DEFAULT,
+        move |msg: rclrs::SerializedMessage| {
+            // Process message and pass it to rerun
+        },
+    )?;
+
     rclrs::spin(node).map_err(|err| err.into())
 }
