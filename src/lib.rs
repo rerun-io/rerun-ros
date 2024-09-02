9dc190cba0eb433e3c31faa474c211e2967d9162
Esteve Fernandez <esteve@apache.org>
2024-09-02 16:19:19 +0200

    Parsing of msg files

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,151 @@
-//! Example of a Rust library.
+use std::collections::HashMap;
+use std::sync::Arc;
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct ROSType {
+    pkg_name: String,
+    msg_name: String,
+}
+
+impl ROSType {
+    pub fn new(pkg_name: &str, msg_name: &str) -> Self {
+        ROSType {
+            pkg_name: pkg_name.to_string(),
+            msg_name: msg_name.to_string(),
+        }
+    }
+
+    pub fn pkg_name(&self) -> &str {
+        &self.pkg_name
+    }
+
+    pub fn msg_name(&self) -> &str {
+        &self.msg_name
+    }
+}
+
+#[derive(Clone)]
+pub struct ROSField {
+    field_type: ROSType,
+}
+
+impl ROSField {
+    pub fn new(field_type: ROSType) -> Self {
+        ROSField { field_type }
+    }
+
+    pub fn type_(&self) -> &ROSType {
+        &self.field_type
+    }
+
+    pub fn change_type(&mut self, new_type: ROSType) {
+        self.field_type = new_type;
+    }
+}
+
+#[derive(Clone)]
+pub struct ROSMessage {
+    msg_type: ROSType,
+    fields: Vec<ROSField>,
+}
+
+impl ROSMessage {
+    pub fn new(def: &str) -> Self {
+        // Placeholder for actual parsing logic
+        ROSMessage {
+            msg_type: ROSType::new("", ""),
+            fields: vec![],
+        }
+    }
+
+    pub fn type_(&self) -> &ROSType {
+        &self.msg_type
+    }
+
+    pub fn set_type(&mut self, new_type: ROSType) {
+        self.msg_type = new_type;
+    }
+
+    pub fn fields(&self) -> &Vec<ROSField> {
+        &self.fields
+    }
+
+    pub fn fields_mut(&mut self) -> &mut Vec<ROSField> {
+        &mut self.fields
+    }
+}
+
+fn split_multiple_message_definitions(multi_def: &str) -> Vec<String> {
+    let mut parts = Vec::new();
+    let mut part = String::new();
+
+    for line in multi_def.lines() {
+        if line.starts_with("========") {
+            parts.push(part.clone());
+            part.clear();
+        } else {
+            part.push_str(line);
+            part.push('\n');
+        }
+    }
+    parts.push(part);
+
+    parts
+}
+
+pub fn parse_message_definitions(multi_def: &str, root_type: &ROSType) -> Vec<Arc<ROSMessage>> {
+    let parts = split_multiple_message_definitions(multi_def);
+    let mut known_type = Vec::new();
+    let mut parsed_msgs = Vec::new();
+
+    let no_type = ROSType::new("", "");
+
+    for i in (0..parts.len()).rev() {
+        let mut msg = Arc::new(ROSMessage::new(&parts[i]));
+
+        if i == 0 {
+            if msg.type_() == &no_type && root_type != &no_type {
+                Arc::get_mut(&mut msg).unwrap().set_type(root_type.clone());
+            } else if msg.type_() == &no_type && root_type == &no_type {
+                println!("{}", multi_def);
+                panic!("Message type unspecified");
+            }
+        }
+
+        parsed_msgs.push(msg.clone());
+        known_type.push(msg.type_().clone());
+    }
+
+    for mut msg in &mut parsed_msgs {
+        for mut field in Arc::get_mut(msg).unwrap().fields_mut() {
+            if field.type_().pkg_name().is_empty() {
+                let mut guessed_type = Vec::new();
+
+                for known_type in &known_type {
+                    if field.type_().msg_name() == known_type.msg_name() {
+                        guessed_type.push(known_type.clone());
+                    }
+                }
+
+                if !guessed_type.is_empty() {
+                    let mut better_match = false;
+
+                    for guessed_type in &guessed_type {
+                        if guessed_type.pkg_name() == root_type.pkg_name() {
+                            field.change_type(guessed_type.clone());
+                            better_match = true;
+                            break;
+                        }
+                    }
+
+                    if !better_match {
+                        field.change_type(guessed_type[0].clone());
+                    }
+                }
+            }
+        }
+    }
+
+    parsed_msgs.reverse();
+    parsed_msgs
+}
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,80 @@ use std::env;
 
 use anyhow::{Error, Result};
 
+const POSE_STAMPED_DEF: &str = "# A Pose with reference coordinate frame and timestamp\n\
+                                Header header\n\
+                                Pose pose\n\
+                                \n\
+                                ===================================================\
+                                ==============================\n\
+                                MSG: std_msgs/Header\n\
+                                # Standard metadata for higher-level stamped data types.\n\
+                                # This is generally used to communicate timestamped data \n\
+                                # in a particular coordinate frame.\n\
+                                # \n\
+                                # sequence ID: consecutively increasing ID \n\
+                                uint32 seq\n\
+                                #Two-integer timestamp that is expressed as:\n\
+                                # * stamp.sec: seconds (stamp_secs) since epoch (in Python the variable is called 'secs')\n\
+                                # * stamp.nsec: nanoseconds since stamp_secs (in Python the variable is called 'nsecs')\n\
+                                # time-handling sugar is provided by the client library\n\
+                                time stamp\n\
+                                #Frame this data is associated with\n\
+                                string frame_id\n\
+                                \n\
+                                ===================================================\
+                                ==============================\n\
+                                MSG: geometry_msgs/Pose\n\
+                                # A representation of pose in free space, composed of position and orientation. \n\
+                                Point position\n\
+                                Quaternion orientation\n\
+                                \n\
+                                ===================================================\
+                                ==============================\n\
+                                MSG: geometry_msgs/Point\n\
+                                # This contains the position of a point in free space\n\
+                                float64 x\n\
+                                float64 y\n\
+                                float64 z\n\
+                                \n\
+                                ===================================================\
+                                ==============================\n\
+                                MSG: geometry_msgs/Quaternion\n\
+                                # This represents an orientation in free space in quaternion form.\n\
+                                \n\
+                                float64 x\n\
+                                float64 y\n\
+                                float64 z\n\
+                                float64 w\n";
+
 fn main() -> Result<(), Error> {
-    let context = rclrs::Context::new(env::args())?;
+    let msg_parsed = rerun_ros::parse_message_definitions(
+        POSE_STAMPED_DEF,
+        &rerun_ros::ROSType::new("geometry_msgs", "PoseStamped"),
+    );
 
-    let node = rclrs::create_node(&context, "minimal_subscriber")?;
+    for msg in msg_parsed {
+        println!("Message: {:?}", msg.type_().pkg_name());
+        println!("Message: {:?}", msg.type_().msg_name());
+        for field in msg.fields() {
+            println!("Field: {:?}", field.type_().pkg_name());
+            println!("Field: {:?}", field.type_().msg_name());
+        }
+    }
+    // let context = rclrs::Context::new(env::args())?;
 
-    let generic_subscription = node.create_generic_subscription(
-        "topic",
-        "std_msgs/msg/String",
-        rclrs::QOS_PROFILE_DEFAULT,
-        move |msg: rclrs::SerializedMessage| {
-            // Process message and pass it to rerun
-            println!("Serialized message: {:?}", msg);
-        },
-    )?;
+    // let node = rclrs::create_node(&context, "minimal_subscriber")?;
 
-    rclrs::spin(node).map_err(|err| err.into())
+    // let generic_subscription = node.create_generic_subscription(
+    //     "topic",
+    //     "std_msgs/msg/String",
+    //     rclrs::QOS_PROFILE_DEFAULT,
+    //     move |msg: rclrs::SerializedMessage| {
+    //         // Process message and pass it to rerun
+    //         println!("Serialized message: {:?}", msg);
+    //     },
+    // )?;
+
+    // rclrs::spin(node).map_err(|err| err.into())
+    Ok(())
 }
