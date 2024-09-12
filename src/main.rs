d64c441ae84d06edfc4d99cd4f622037e726bd32
Esteve Fernandez <esteve@apache.org>
2024-09-12 15:45:53 +0200

    Added subscriptions to every item in the configuration file. Fix types in the configuration file
    Signed-off-by: Esteve Fernandez <esteve@apache.org>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::{Error, Result};
 use clap::Parser;
 use rerun_ros::config::ConfigParser;
+use std::env;
+use std::sync::Arc;
 
 /// A bridge between rerun and ROS
 #[derive(Parser, Debug)]
@@ -20,6 +22,24 @@ fn main() -> Result<(), Error> {
 
     println!("Starting bridge");
     let config_parser = ConfigParser::new(&bridge_args.config_file)?;
-    println!("{:?}", config_parser.conversions());
+
+    let context = rclrs::Context::new(env::args())?;
+    let node = rclrs::create_node(&context, "rerun_ros_bridge")?;
+    for ((topic_name, _frame_id), (ros_type, _converter)) in config_parser.conversions() {
+        let msg_spec = rerun_ros::ros_introspection::MsgSpec::new(ros_type)?;
+
+        println!(
+            "Subscribing to topic: {topic_name} with type: {ros_type}"
+        );
+        let _generic_subscription = node.create_generic_subscription(
+            topic_name,
+            ros_type,
+            rclrs::QOS_PROFILE_DEFAULT,
+            move |_msg: rclrs::SerializedMessage| {
+                let _msg_spec = Arc::new(&msg_spec);
+                // Process message and pass it to rerun
+            },
+        )?;
+    }
     Ok(())
 }
